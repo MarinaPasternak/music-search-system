8c8e60d96d33a259ff0e9395a2f54c2c1cb03ed4
Marina_Pasternak <256pasternakmarina@gmail.com>
2023-04-24 21:34:24 +0300

    added scss bootstrapVue

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap';
+import './assets/scss/main.scss';
 
 createApp(App).mount('#app')
