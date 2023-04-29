8e9fb3e87658f63f041c43a3d67b187ebca55722
Marina_Pasternak <256pasternakmarina@gmail.com>
2023-04-29 22:55:07 +0300

    added basic structure for Search and Statistic components

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,14 @@
 import SignUpForm from "../views/signUp/SignUpForm.vue";
 import SignInForm from "../views/signIn/SignInForm.vue";
+import SearchMusicComponent from "../views/searchMusic/SearchMusicComponent.vue";
+import StatisticComponent from"../views/statistic/StatisticComponent.vue";
 import NotFound from '../components/NotFoundedComponent.vue';
 
 export const routes = [
     {
         path: '/',
         name: 'home-page',
-        component: SignInForm
+        component: SearchMusicComponent
     }, {
         path: '/sign-in',
         name: 'sign-in',
@@ -15,6 +17,10 @@ export const routes = [
         path: '/sign-up',
         name: 'sign-up',
         component: SignUpForm,
+    }, {
+        path: '/statistic',
+        name: 'statistic',
+        component: StatisticComponent,
     }, {
         path: '/:pathMatch(.*)*',
         name: 'not-found',
---
+++ src/views/searchMusic/SearchMusicComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Search Music</h1>
+    <button class="primary-button">See Statistic</button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/styles/main.scss";
+</style>
