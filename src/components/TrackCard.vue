2f525ff31ce28999028827f9742fc7f7065882e2
Marina_Pasternak <256pasternakmarina@gmail.com>
2023-05-01 21:52:43 +0300

    added card component to represent a track

---
+++ src/components/TrackCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="track-card">
+    <p>{{ postionInChart + 1 }}</p>
+    <h4>{{ track.name }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TrackCard",
+  props: ["postionInChart", "track"],
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/searchMusic/components/TopTracksChartComponent.vue
+++ src/views/searchMusic/components/TopTracksChartComponent.vue
@@ -1,12 +1,28 @@
 <template>
   <div v-if="topTracks">
-    {{ topTracks }}
+    <div class="cart-container">
+      <h2 class="gradient-title">Top 50 Global</h2>
+      <div clas="flex-container">
+        <track-card
+          v-for="(track, index) in topTracks"
+          :key="index"
+          :postionInChart="index"
+          :track="track"
+        ></track-card>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { mapState, mapActions } from "vuex";
+
+import TrackCard from "@/components/TrackCard.vue";
 export default {
+  name: "TopTracksChartComponent",
+  components: {
+    TrackCard,
+  },
   computed: {
     ...mapState({
       topTracks: (state) => state.topTracks.topTracks,
@@ -21,4 +37,6 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import "@/assets/styles/main.scss";
+</style>
