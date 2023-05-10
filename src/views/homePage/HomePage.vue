3a733d65dae069544ed573abea70ca1a21f6c659
Marina_Pasternak <256pasternakmarina@gmail.com>
2023-05-10 17:13:41 +0300

    updated function for create recomandation

--- src/views/homePage/HomePage.vue
+++ src/views/homePage/HomePage.vue
@@ -3,7 +3,7 @@
     <h1 class="gradient-title">{{ fullName }}</h1>
     <music-search :passedQuery="passedQuery"></music-search>
     <liked-songs-component></liked-songs-component>
-    <!-- <recomended-songs-component></recomended-songs-component> -->
+    <recomended-songs-component></recomended-songs-component>
     <top-artist-component></top-artist-component>
     <top-tags-component @getTagName="searchByTag"></top-tags-component>
     <top-tracks-chart-component></top-tracks-chart-component>
@@ -18,7 +18,7 @@ import TopArtistComponent from "./components/TopArtistComponent.vue";
 import TopTagsComponent from "./components/TopTagsComponent.vue";
 import MusicSearch from "../musicSearch/MusicSearch.vue";
 import LikedSongsComponent from "./components/LikedSongsComponent.vue";
-// import RecomendedSongsComponent from "./components/RecomendedSongsComponent.vue";
+import RecomendedSongsComponent from "./components/RecomendedSongsComponent.vue";
 
 export default {
   name: "HomeComponent",
@@ -28,7 +28,7 @@ export default {
     TopTagsComponent,
     MusicSearch,
     LikedSongsComponent,
-    // RecomendedSongsComponent,
+    RecomendedSongsComponent,
   },
   data() {
     return {
@@ -72,101 +72,3 @@ export default {
   }
 }
 </style>
-
-<!-- <template>
-  <div>
-    <h2>Recommended Songs</h2>
-    <ul>
-      <li v-for="song in recommendedSongs" :key="song.name">
-        {{ song.name }} by {{ song.artist }}
-      </li>
-    </ul>
-  </div>
-</template>
-
-<script>
-import axios from "axios";
-// import { getScoreDescription } from "@/utils/rules";
-
-const rules = {
-  affective: [
-    { score: 0, description: "Sad" },
-    { score: 5, description: "Neutral" },
-    { score: 10, description: "Happy" },
-  ],
-  structural: [
-    { score: 0, description: "Simple" },
-    { score: 5, description: "Balanced" },
-    { score: 10, description: "Complex" },
-  ],
-  kinesthetic: [
-    { score: 0, description: "Still" },
-    { score: 5, description: "Moderate" },
-    { score: 10, description: "Dynamic" },
-  ],
-};
-
-const API_KEY = process.env.VUE_APP_LAST_FM_API_KEY;
-
-export default {
-  data() {
-    return {
-      recommendedSongs: [],
-    };
-  },
-  methods: {
-    async getRecommendedSongs() {
-      const response = await axios.get("https://ws.audioscrobbler.com/2.0/", {
-        params: {
-          method: "track.getSimilar",
-          artist: "Radiohead", // replace with user input
-          track: "Paranoid Android", // replace with user input
-          api_key: API_KEY,
-          format: "json",
-        },
-      });
-
-      const recommendedSongs = response.data.similartracks.track.map(
-        (track) => {
-          const { name, artist } = track;
-          const {
-            affective = 5,
-            structural = 5,
-            kinesthetic = 5,
-          } = track.recommendation || {};
-          return {
-            name,
-            artist,
-            affective: {
-              score: affective,
-              description: rules.affective,
-            },
-            structural: {
-              score: structural,
-              description: rules.structural,
-            },
-            kinesthetic: {
-              score: kinesthetic,
-              description: rules.kinesthetic,
-            },
-          };
-        }
-      );
-
-      console.log(recommendedSongs);
-
-      this.recommendedSongs = recommendedSongs;
-    },
-    getQueryParams() {
-      const affective = parseInt(this.affectiveInput);
-      const structural = parseInt(this.structuralInput);
-      const kinesthetic = parseInt(this.kinestheticInput);
-
-      return { affective, structural, kinesthetic };
-    },
-  },
-  created() {
-    this.getRecommendedSongs();
-  },
-};
-</script> -->
--- src/views/homePage/components/RecomendedSongsComponent.vue
+++ src/views/homePage/components/RecomendedSongsComponent.vue
@@ -47,6 +47,23 @@ export default {
       offset: 0,
       rowsPerPage: 5,
       loading: false,
+      rules: {
+        affective: [
+          { score: 0, description: "Sad" },
+          { score: 5, description: "Neutral" },
+          { score: 10, description: "Happy" },
+        ],
+        structural: [
+          { score: 0, description: "Simple" },
+          { score: 5, description: "Balanced" },
+          { score: 10, description: "Complex" },
+        ],
+        kinesthetic: [
+          { score: 0, description: "Still" },
+          { score: 5, description: "Moderate" },
+          { score: 10, description: "Dynamic" },
+        ],
+      },
     };
   },
   computed: {
@@ -69,17 +86,6 @@ export default {
         } else {
           this.likedSongs = [];
         }
-        this.likedSongs = await Promise.all(
-          this.likedSongs.map(async (item) => {
-            const [songTitle, artistName] = item.split("~");
-            const response = await axios.get(
-              `https://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=${API_KEY}&artist=${artistName}&track=${songTitle}&format=json`
-            );
-            const tags =
-              response.data.track.toptags?.tag?.map((tag) => tag.name) || [];
-            return { songTitle, artistName, tags };
-          })
-        );
       });
     },
     async getRecommendedSongs() {
@@ -93,48 +99,74 @@ export default {
 
         // Get a list of recommended songs based on the user's liked songs
         const recommendedSongs = await this.getSimilarSongs(this.likedSongs);
+        console.log(recommendedSongs);
 
         // Calculate the similarity score of each recommended song based on its tags
         const tagSimilarities = recommendedSongs.map((song) => {
-          if (song && song.tags) {
-            const userTags = this.likedSongs.flatMap((s) => s.tags);
+          if (song) {
+            const userLikes = this.likedSongs;
+            const songRules = {
+              affective: song.affective,
+              structural: song.structural,
+              kinesthetic: song.kinesthetic,
+            };
 
-            const songTags = song.tags;
+            // calculate similarity based on rules
+            const similarityScore = Object.keys(songRules).map((category) => {
+              const userCategoryScore = Math.max(
+                ...userLikes.map((like) => like[category].score)
+              );
 
-            // Calculate the Jaccard similarity coefficient between the user tags and the song tags
-            const intersection = new Set(
-              userTags.filter((tag) => songTags.includes(tag))
-            );
-            const union = new Set([...userTags, ...songTags]);
-            const similarityScore = intersection.size / union.size;
+              console.log(userCategoryScore);
+
+              let songCategoryScore = 0;
+
+              let sum = 0;
+              let count = 0;
+              for (let key in songRules) {
+                const rule = songRules[key];
+                const fuzzyScore = this.fuzzyLogic(
+                  rule.score,
+                  userCategoryScore
+                );
+                sum += fuzzyScore;
+                count++;
+              }
+              songCategoryScore = sum / count;
+              console.log("songCategoryScore", songCategoryScore);
+
+              return songCategoryScore;
+            });
 
             return {
               name: song.title,
               artist: { name: song.artist },
-              tags: songTags,
+              rules: songRules,
               playcount: song.playcount,
               similarityScore,
+              tags: song.tags,
             };
           }
         });
 
         // Filter the recommended songs based on their similarity to the user's liked songs
-        this.recommendedSongs = tagSimilarities
-          .filter((song) => {
-            if (song && song.similarityScore > 0) {
-              return song;
-            }
-          })
-          .sort((a, b) => b.similarityScore - a.similarityScore);
+        this.recommendedSongs = tagSimilarities.filter((song) => {
+          if (
+            song &&
+            !song.similarityScore.every((score) => isNaN(score) || score === 0)
+          ) {
+            return song;
+          }
+        });
 
-        // console.log(this.recommendedSongs);
+        console.log("this.recommendedSongs", this.recommendedSongs);
 
         // Log the similarity score of each recommended song
-        // this.recommendedSongs.forEach((song) => {
-        //   console.log(
-        //     `${song.title} - ${song.artist}: ${song.similarityScore}`
-        //   );
-        // });
+        this.recommendedSongs.forEach((song) => {
+          console.log(
+            `${song.title} - ${song.artist}: ${song.similarityScore}`
+          );
+        });
       } catch (error) {
         console.error(error);
       }
@@ -147,7 +179,7 @@ export default {
         // Loop through each seed song and make a request to the Last.fm API to get a list of similar songs
         for (const song of seedSongs) {
           const response = await axios.get(
-            `http://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=${song.artistName}&track=${song.songTitle}&api_key=${API_KEY}&limit=10000&format=json`
+            `http://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=${song.artist}&track=${song.title}&api_key=${API_KEY}&limit=100&format=json`
           );
 
           // Extract the list of similar songs from the response and add them to the overall list
@@ -155,7 +187,7 @@ export default {
             const songs = response.data.similartracks.track.map(
               async (track) => {
                 const trackResponse = await axios.get(
-                  `https://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=${API_KEY}&artist=${track.artist.name}&track=${track.name}&format=json`
+                  `https://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=${API_KEY}&artist=${song.artist}&track=${song.title}&format=json`
                 );
 
                 let tags = null;
@@ -168,8 +200,29 @@ export default {
                   return {
                     title: track.name,
                     artist: track.artist.name,
-                    tags: tags,
                     playcount: track.playcount,
+                    affective: {
+                      score: Math.floor(Math.random() * 10) + 1,
+                      description: this.getDescription(
+                        Math.floor(Math.random() * 10) + 1,
+                        this.rules.affective
+                      ),
+                    },
+                    structural: {
+                      score: Math.floor(Math.random() * 10) + 1,
+                      description: this.getDescription(
+                        Math.floor(Math.random() * 10) + 1,
+                        this.rules.structural
+                      ),
+                    },
+                    kinesthetic: {
+                      score: Math.floor(Math.random() * 10) + 1,
+                      description: this.getDescription(
+                        Math.floor(Math.random() * 10) + 1,
+                        this.rules.kinesthetic
+                      ),
+                    },
+                    tags: tags,
                   };
                 }
               }
@@ -184,6 +237,69 @@ export default {
       } catch (error) {
         console.error(error);
       }
+    },
+    fuzzyLogic(ruleScore, userCategoryScore) {
+      console.log("ruleScore", ruleScore);
+      // Find the rule with the closest score to the given score
+      const membershipFunctions = {
+        low: (x) =>
+          Math.max(0, Math.min((x - 0) / (5 - 0), (10 - x) / (10 - 5))),
+        medium: (x) =>
+          Math.max(0, Math.min((x - 5) / (10 - 5), (10 - x) / (10 - 5))),
+        high: (x) =>
+          Math.max(0, Math.min((x - 5) / (10 - 5), (10 - x) / (5 - 0))),
+      };
+
+      const membershipLevels = {
+        high: membershipFunctions.high(userCategoryScore),
+        medium: membershipFunctions.medium(userCategoryScore),
+        low: membershipFunctions.low(userCategoryScore),
+      };
+
+      console.log("membershipLevels", membershipLevels);
+
+      // Determine the degree of membership of the rule score in each category
+      const degreesOfMembership = {
+        low: membershipFunctions.low(ruleScore),
+        medium: membershipFunctions.medium(ruleScore),
+        high: membershipFunctions.high(ruleScore),
+      };
+
+      // Determine the degree of support for each category
+      const degreesOfSupport = {
+        low: Math.min(membershipLevels.low, degreesOfMembership.low),
+        medium: Math.min(membershipLevels.medium, degreesOfMembership.medium),
+        high: Math.min(membershipLevels.high, degreesOfMembership.high),
+      };
+
+      console.log("degreesOfSupport", degreesOfSupport);
+
+      // Determine the total degree of support for all categories
+      const totalDegreeOfSupport =
+        degreesOfSupport.low + degreesOfSupport.medium + degreesOfSupport.high;
+
+      console.log("totalDegreeOfSupport", totalDegreeOfSupport);
+
+      // Determine the centroid of the support
+      const centroid =
+        (degreesOfSupport.low * 2 +
+          degreesOfSupport.medium * 5 +
+          degreesOfSupport.high * 8) /
+        totalDegreeOfSupport;
+
+      console.log("centroid", centroid);
+
+      return centroid;
+    },
+    getDescription(score, rules) {
+      // Find the rule with the closest score to the given score
+      const rule = rules.reduce((prev, curr) =>
+        Math.abs(curr.score - score) < Math.abs(prev.score - score)
+          ? curr
+          : prev
+      );
+
+      return rule.description;
     },
   },
   watch: {
